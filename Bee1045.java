c17cce19ec29954654bad9a0dccfc6ecf7ec0bba
Arthur <arthur.ramalho.freire@gmail.com>
2024-09-09 19:46:46 -0300

    first: mais um exercicio resolvido

--- Bee1045.java
+++ Bee1045.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.Arrays;
 
@@ -11,6 +12,31 @@ public class Bee1045 {
         b = sc.nextDouble();
         c = sc.nextDouble();
 
-        double[] numeros = {a, b, c};
+        Double[] lados = {a, b, c};
+        Arrays.sort(lados, Collections.reverseOrder());
+
+        a = lados[0];
+        b = lados[1];
+        c = lados[2];
+
+        if (a >= b + c) {
+            System.out.println("NAO FORMA TRIANGULO");
+        } else {
+            if (a * a == b * b + c * c) {
+                System.out.println("TRIANGULO RETANGULO");
+            }
+            if (a * a > b * b + c * c) {
+                System.out.println("TRIANGULO OBTUSANGULO");
+            }
+            if (a * a < b * b + c * c) {
+                System.out.println("TRIANGULO ACUTANGULO");
+            }
+            if (a == b && b == c) {
+                System.out.println("TRIANGULO EQUILATERO");
+            }
+            if ((a == b && b != c) || (a == c && b != c) || (b == c && a != b)) {
+                System.out.println("TRIANGULO ISOSCELES");
+            }
+        }
     }
 }
